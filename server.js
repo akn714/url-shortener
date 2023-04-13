43556096627c248d827bd714feef70303999ec2d
adarshkumar714 <123639529+adarshkumar714@users.noreply.github.com>
2023-04-13 14:30:10 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,9 +7,8 @@ app.use('/static', express.static(path.join(__dirname, 'static')));
 
 
 let get_unique_id = () => {
-  shortUrl = Math.floor((1 + Math.random()) * 0x10000)
-      .toString(16)
-      .substring(1);
+  shortUrl = Math.floor((1 + Math.random()) * 0x1000000).toString(16).substring(1);
+    return shortUrl
 }
 
 console.log(get_unique_id());
@@ -20,10 +19,15 @@ app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname, '/index.html'));
 })
 
-app.post('/', (req, res)=>{
-    
+app.post('/shorturl', (req, res)=>{
+
     // code incomplete
-    
+//    let url = req.body.url;
+    let shorturl = get_unique_id();
+    app.get(`/${shorturl}`, (req, res)=>{
+        res.redirect(url);
+    });
+    res.send(`your site is now running at https://urlshortener.adarshkumar35.repl.co/${shorturl}`);
 })
 
 app.listen(5000, (req, res)=>{
